a5885bdec2ef4e68cabf9fa0a0efc1e109a0a105
Andrea Zonca <code@andreazonca.com>
2023-12-07 23:02:19 -0800

    updated scripts for dc0 final release

--- get-dc0-file-lists.sh
+++ get-dc0-file-lists.sh
@@ -5,23 +5,25 @@
 # but it will be easier to work with the individual grpups of files
 # already broken out
 
-mission=chlat
+ENDPOINT='c9dc477a-3db5-4946-874d-a5dc7efcabcf'
+DOMAIN='g-456d30.0ed28.75bc' # NERSC`
 
-for split in 01 02
+for mission in chlat splat spsat
 do
-    for band in 025 040 090 150 230 280
+    for split in 01 02 04 08 16 32
     do
-	# globus ls -lrF json  38f01147-f09e-483d-a552-3866669a846d:/datareleases/dc0/mission/chlat/split$split/$band/ > dc0-chlat-split$split-$band.json
-	globus ls -lrF json  38f01147-f09e-483d-a552-3866669a846d:/inbox/dc0_output/$mission/split$split/$band/ > dc0-$mission-split$split-$band.json
-    done
-done
-
-mission=spsat
-
-for split in 01 02
-do
-    for band in 025 040 085 095 145 155 230 280
-    do
-	globus ls -lrF json  38f01147-f09e-483d-a552-3866669a846d:/inbox/dc0_output/$mission/split$split/$band/ > dc0-$mission-split$split-$band.json
+        echo $mission $split
+        BANDS="025 040 085 095 145 155 230 280" #spsat
+        if [ "$mission" = "chlat" ]; then
+            BANDS="025 040 090 150 230 280"
+        fi
+        if [ "$mission" = "splat" ]; then
+            BANDS="020 025 040 090 150 230 280"
+        fi
+        for band in $BANDS
+        do
+        # globus ls -lrF json  38f01147-f09e-483d-a552-3866669a846d:/datareleases/dc0/mission/chlat/split$split/$band/ > dc0-chlat-split$split-$band.json
+        globus ls -lrF json $ENDPOINT:/datareleases/dc0/mission/$mission/split$split/$band/ > dc0-$mission-split$split-$band.json
+        done
     done
 done
