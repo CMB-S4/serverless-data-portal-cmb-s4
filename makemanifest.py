6bf2113b3da632a357c33404a632ba8fe03514f8
Andrea Zonca <code@andreazonca.com>
2025-04-22 10:18:50 -0700

    use cmb collection and handle folder as argument

--- makemanifest.py
+++ makemanifest.py
@@ -26,32 +26,39 @@ import json
 import glob
 import hashlib
 
-baseurl = sys.argv[1]
+folder = sys.argv[1]
+# CMB collection at NERSC
+baseurl = "https://g-0a470a.6b7bd8.0ec8.data.globus.org/"
 # example NERSC
 # baseurl = "https://g-9fdb0b.6b7bd8.0ec8.data.globus.org/datareleases/dc0/mission/"
 # example UCSD
 # baseurl = 'https://g-456d30.0ed28.75bc.data.globus.org/datareleases/npipe6v20/fullsky/'
 
-BUFFER = 4*1073741824
+BUFFER = 4 * 1073741824
 
-for dirpath, dirnames, filenames in os.walk("."):
+for dirpath, dirnames, filenames in os.walk(folder):
+    print(dirpath)
     manifest_dict = {}
     for filename in filenames:
-        path = os.path.join(dirpath[2:], filename)
-        sha512 = hashlib.sha512()
-        with open(path, "rb") as f:
-            while True:
-                data = f.read(BUFFER)
-                if not data:
-                    break
-                sha512.update(data)
-        length = os.stat(path).st_size
-        manifest_dict[path] = {'sha512': sha512.hexdigest(),
-                            'filename': path,
-                                'url': baseurl + path,
-                            'length': length}
+        if filename not in ["manifest.json"]:
+            print(filename)
+            path = os.path.join(dirpath, filename)
+            sha512 = hashlib.sha512()
+            with open(path, "rb") as f:
+                while True:
+                    data = f.read(BUFFER)
+                    if not data:
+                        break
+                    sha512.update(data)
+            length = os.stat(path).st_size
+            manifest_dict[path] = {
+                "sha512": sha512.hexdigest(),
+                "filename": os.path.basename(path),
+                "url": baseurl + path,
+                "length": length,
+            }
 
     if len(filenames) > 0:
-        with open(os.path.join(dirpath, 'manifest.json'), 'w') as f:
+        with open(os.path.join(dirpath, "manifest.json"), "w") as f:
             json.dump(list(manifest_dict.values()), f, indent=4)
         print(dirpath)
